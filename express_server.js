262b68fc2d5b9e53e0544b6dfe5bff475157d8dc
Jeffrey Lee <jjlee16@gmail.com>
2018-04-18 18:26:24 +0000

    added cookie functionality

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser')
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 app.set("view engine", "ejs");
 
 function generateRandomString() {
@@ -25,8 +27,24 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+
+//-------------Login-------------------
+app.post("/login", (req, res) => {
+  var loginName = req.body.username;
+  res.cookie('username',{ username: loginName});
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
+//-------------URLS--------------------
 app.get("/urls", (req, res) => {
-  let urls = { urls: urlDatabase };
+  let username = req.cookies["username"];
+  let urls = { urls: urlDatabase, username: username };
+  //console.log(username);
   res.render("urls_index", urls);
 });
 
@@ -37,22 +55,15 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let username = req.cookies["username"];
+  let name = {username: username};
+  res.render("urls_new", name);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURLs: req.params.id, fullURLs: urlDatabase[req.params.id]};
+  let username = req.cookies["username"];
+  let templateVars = { shortURLs: req.params.id, fullURLs: urlDatabase[req.params.id], username: username};
   res.render("urls_show", templateVars);
-});
-
-app.post("/urls/:id/update", (req, res) => {
-  urlDatabase[req.params.id] = req.body.fullURL;
-  res.redirect("/urls");
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect(`/urls`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -65,6 +76,17 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+//--------------Ediing URLS------------------
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.fullURL;
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect(`/urls`);
 });
 
 app.listen(PORT, () => {
